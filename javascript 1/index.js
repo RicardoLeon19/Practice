e5d95240fc4836bfe5bd43229df26c312b101889
RicardoLeon19 <ricardoleonrdz19@gmail.com>
2022-06-26 23:10:54 -0500

    Comentario

--- javascript 1/index.js
+++ javascript 1/index.js
@@ -1,3 +1,5 @@
+
+//First Exercise
 let sale = true;
 
 sale = false;
@@ -8,3 +10,22 @@ if(sale) {
 else { 
   console.log('Time to wait for a sale');
   }
+//Second exercise
+
+let hungerLevel = 8;
+if(hungerLevel > 7 ){
+  console.log('Time to eat!')
+} else {
+  console.log('We can eat later!')
+}
+
+//Third exercise
+let mood = 'sleepy';
+let tirednessLevel = 6;
+if (mood==='sleepy' && tirednessLevel > 8){
+  console.log('time to sleep')  
+}
+else {
+  console.log('not bed time yet')
+}
+
